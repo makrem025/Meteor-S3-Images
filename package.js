01bdd25401e2facb5c9ef8eab5cc354098f7c082
Martin A. Midtsund <martin@iterate.no>
2014-01-15 11:05:55 +0100

    Fixed wrong path on files created by previous commit

--- package.js
+++ package.js
@@ -10,8 +10,8 @@ Package.on_use(function (api) {
 	//Need service-configuration to use Meteor.method
 	api.use(["underscore", "ejson","service-configuration"], ["client", "server"]);
 	api.use(["handlebars", "spark"], "client");
-	api.add_files("s3client.js", "client");
-	api.add_files("s3server.js", "server");
+	api.add_files("lib/client/s3client.js", "client");
+	api.add_files("lib/server/s3server.js", "server");
 
 	//Allows user access to Knox
 	api.export && api.export("Knox","server");
